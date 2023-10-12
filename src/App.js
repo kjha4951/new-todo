9b487a01fd0c9d9864753cfc87b17c122a7846d1
kjha4951 <kjha4951@gmail.com>
2023-10-12 15:50:00 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { ToDolist } from './component/todolist';
+
+const App = () => {
+  const [InputList, setInputList] = useState("");
+  const [items, setItems] = useState([]);
+  const [editIndex, setEditIndex] = useState(-1);
+
+  const ItemEvent = (e) => {
+    setInputList(e.target.value);
+  };
+  const addOrUpdateItem = (e) => {
+    e.preventDefault();
+    if (editIndex !== -1) {
+      items[editIndex] = InputList;
+      setItems([...items]);
+      setEditIndex(-1);
+    } else {
+      setItems([...items, InputList]);
+    }
+    setInputList("");
+  };
+
+  const editItem = (index) => {
+    setInputList(items[index]);
+    setEditIndex(index);
+  };
+
+  const deleteItem = (index) => {
+    const updatedItems = [...items];
+    updatedItems.splice(index, 1);
+    setItems(updatedItems);
+  };
+
+  return (
+    <div className='main_div'>
+      <div className='code_div'>
+        <br />
+        <h1>ToDo List</h1>
+        <br />
+        <form onSubmit={addOrUpdateItem}>
+          <input type='text' placeholder='Add an item' value={InputList} onChange={ItemEvent} />
+          <button type="submit">
+
+            {editIndex !== -1 ? (
+              <span>
+                <i className="bi bi-pencil"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                  <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
+                </svg></i>
+              </span>
+            ) : (
+              <span>
+                <i className="bi bi-plus"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-plus-fill" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5v-.5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0zM8.5 8a.5.5 0 0 0-1 0v1.5H6a.5.5 0 0 0 0 1h1.5V12a.5.5 0 0 0 1 0v-1.5H10a.5.5 0 0 0 0-1H8.5V8z" />
+                </svg></i>
+              </span>
+            )}
+
+          </button>
+        </form>
+        <ol className='ol-main'>
+          {items.map((itemval, index) => (
+            <div key={index}>
+              <ToDolist text={itemval} />
+              <button onClick={() => editItem(index)}><i class="bi bi-pencil"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
+              </svg></i></button>
+              <button onClick={() => deleteItem(index)} className='delete-button'><i class="bi bi-trash3-fill"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
+              </svg></i></button>
+            </div>
+          ))}
+        </ol>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/todolist.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const ToDolist=(props)=>{
+    return <li>{props.text}</li>
+};
